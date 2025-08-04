3e35d095bac9ab6451a03f0457569395670a3ba8
VictorAkashReznov <akashkushwaha5956@gmail.com>
2025-08-04 20:37:06 +0530

    cors error fron frontend is resolved.

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const expenseRoute = require("./routes/expense");
 dotenv.config();
 const app = express();
 // MIDDLEWERE
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://expense-tracker-frontend-five-sable.vercel.app/", // exact frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
 // ROUTES
 app.use("/expenses", expenseRoute);
